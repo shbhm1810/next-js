3926114b539f9de189c3a01d86607364a79cf7ad
shbhm1810 <shubhamsharma81327@gmail.com>
2023-04-25 14:29:00 +0530

    TUT! Dynamic routing

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
 
 import { getSortedPostsData } from '../lib/posts';
 
@@ -22,7 +24,7 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+               <Link href={`/posts/${id}`}>{title}</Link>
               <br />
               {id}
               <br />
